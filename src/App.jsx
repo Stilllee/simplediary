546a5d125e67ff5e4e2fb7323ec0abfdee91ba8f
Stilllee <orodae@gmail.com>
2023-08-20 19:25:15 +0900

    리스트 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "Woodstock",
+    content: "What a Nice Day!",
+    emotion: 4,
+    created_date: new Date().getTime(), // getTime()은 현재 시간을 숫자(milliseconds)로 반환함
+  },
+  {
+    id: 2,
+    author: "Snoopy",
+    content: "I'm so happy",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "Charlie Brown",
+    content: "Gloomy Day :(",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
---
+++ src/DiaryList.jsx
@@ -0,0 +1,22 @@
+import DiaryItem from "./DiaryItem";
+
+const DiaryList = ({ diaryList }) => {
+  return (
+    <div className="DiaryList">
+      <h2>일기 리스트</h2>
+      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+      <div>
+        {diaryList.map((it) => (
+          <DiaryItem key={it.id} {...it} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// undefined가 들어올 수도 있기 때문에 defaultProps를 통해 기본값을 설정해줌
+DiaryList.defaultProps = {
+  diaryList: [], // 기본값을 빈 배열로 설정
+};
+
+export default DiaryList;
